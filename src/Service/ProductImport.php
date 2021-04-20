16bdf9baf4a00a587faf84b6d2ff4238afd36e56
Dorota <d.a.lewinska@gmail.com>
2021-04-20 11:17:37 +0100

    Added function for creating product object

--- src/Service/ProductImport.php
+++ src/Service/ProductImport.php
@@ -17,5 +17,19 @@ class ProductImport
 		$this->arrConfig = $arrConfig;
 		$this->csvImport = $objCsvImport;
 	}
-
+	
+	/** 
+	 * this turns csv product into an object (fills in empty object Product form model class)
+	 */
+	public function createProductFromData(array $arrProduct)
+	{
+		$objProduct = new Product();
+		$objProduct->setCode($arrProduct['Product Code']);
+		$objProduct->setName($arrProduct['Product Name']);
+		$objProduct->setDescription($arrProduct['Product Description']);
+		$objProduct->setStock($arrProduct['Stock']);
+		$objProduct->setPrice(preg_replace("/[^0-9.]/", "", $arrProduct['Cost in GBP'])); //strip non-numeric
+		$objProduct->setDiscontinued($arrProduct['Discontinued']);
+		return $objProduct;
+	}
 }
