a84c0fb109fbebf0087732e81d4d4f01056f0d43
Dorota <d.a.lewinska@gmail.com>
2021-04-20 11:13:34 +0100

    Added test for creating product object.

--- tests/ProductImportTest.php
+++ tests/ProductImportTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tests;
 
 require_once __DIR__ . '/../bootstrap.php';
@@ -14,14 +15,31 @@ class ProductImportTest extends TestCase
         $container = new Container($arrConfig);
         return $container;
     }
-    
+
     /**
-     * A basic test example.
-     *
-     * @return void
+     * Test making objProduct
      */
-    public function testBasicTest()
+    public function testCreateProductFromData()
     {
-        $this->assertTrue(true);
+        $container = $this->makeContainer();
+        $objImportProduct = $container->getProductImport();
+        $arrProduct = [
+            'Product Code' => 'P0001',
+            'Product Name' => 'TV',
+            'Product Description' => '32” Tv',
+            'Stock' => '10',
+            'Cost in GBP' => '399.99',
+            'Discontinued' => ''
+        ];
+        $objProduct = $objImportProduct->createProductFromData($arrProduct);
+        //echo '<pre>';
+        //print_r($objProduct);
+
+        $this->assertTrue($objProduct->getCode() == $arrProduct['Product Code']);
+        $this->assertTrue($objProduct->getName() == $arrProduct['Product Name']);
+        $this->assertTrue($objProduct->getDescription() == $arrProduct['Product Description']);
+        $this->assertTrue($objProduct->getStock() == $arrProduct['Stock']);
+        $this->assertTrue($objProduct->getPrice() == $arrProduct['Cost in GBP']);
+        $this->assertTrue($objProduct->getDiscontinued() == $arrProduct['Discontinued']);
     }
 }
