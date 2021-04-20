097242d26db39c0e98c3ada7955f804282b98554
Dorota <d.a.lewinska@gmail.com>
2021-04-20 10:29:58 +0100

    Added file with product sample and import test.

--- tests/CsvImportTest.php
+++ tests/CsvImportTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -34,4 +35,53 @@ class CsvImportTest extends TestCase
         $this->assertSame($arrCSVfive, $arrFiveLines);
     }
 
+    /**
+     * This tests a sample file of 4 products
+     */
+    public function testProductSample()
+    {
+        $objCsvImport = new CsvImport();
+
+        $arrResult = [
+            [
+                'Product Code' => 'T0001',
+                'Product Name' => 'TV',
+                'Product Description' => '32” Tv',
+                'Stock' => '10',
+                'Cost in GBP' => '1000.99',
+                'Discontinued' => ''
+            ],
+            [
+                'Product Code' => 'T0002',
+                'Product Name' => 'Cd Player',
+                'Product Description' => 'Nice CD player',
+                'Stock' => '11',
+                'Cost in GBP' => '50.12',
+                'Discontinued' => 'yes'
+            ],
+            [
+                'Product Code' => 'T0003',
+                'Product Name' => 'VCR',
+                'Product Description' => 'Top notch VCR',
+                'Stock' => '9',
+                'Cost in GBP' => '4.33',
+                'Discontinued' => 'yes'
+            ],
+            [
+                'Product Code' => 'T0011',
+                'Product Name' => 'Misc Cables',
+                'Product Description' => 'error in export',
+                'Stock' => '',
+                'Cost in GBP' => '',
+                'Discontinued' => ''
+            ]
+
+        ];
+        //check empty
+        $arrCSV = $objCsvImport->turn_csv_file_into_array('tests/testFiles/productSample.csv');
+        //echo '<pre>';
+        //print_r($arrCSV);
+
+        $this->assertSame($arrCSV, $arrResult);
+    }
 }
